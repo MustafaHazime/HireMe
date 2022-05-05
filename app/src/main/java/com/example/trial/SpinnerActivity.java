44c67b72e4f46a35a154ae9d725cbb82f0dcb712
MustafaHazime <mustafahazime2@gmail.com>
2022-05-05 19:02:09 +0300

    Commit 22

--- app/src/main/java/com/example/trial/SpinnerActivity.java
+++ app/src/main/java/com/example/trial/SpinnerActivity.java
@@ -6,5 +6,14 @@ import android.widget.AdapterView;
 public class SpinnerActivity extends HomePage implements AdapterView.OnItemSelectedListener {
 
 
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
 
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+
+    }
 }
--- app/src/main/java/com/example/trial/redirectory.java
+++ app/src/main/java/com/example/trial/redirectory.java
@@ -9,20 +9,6 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class redirectory extends AppCompatActivity {
-    ImageButton settingsbtn;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_redirectory);
-        settingsbtn= (ImageButton) findViewById(R.id.settings);
-        settingsbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), profile.class);
-                startActivity(intent);
-                Toast.makeText(getApplicationContext(),"Profile Page",Toast.LENGTH_SHORT).show();
 
-            }
-        });
     }
 }
