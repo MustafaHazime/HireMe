8318f27222d7d09e1e131ea64fbbdcd22c6bc756
MustafaHazime <mustafahazime2@gmail.com>
2022-05-05 18:55:50 +0300

    Commit 19

--- app/src/main/java/com/example/trial/singleToneClass.java
+++ app/src/main/java/com/example/trial/singleToneClass.java
@@ -2,18 +2,5 @@ package com.example.trial;
 
 
 public class singleToneClass {
-    String s;
 
-    private static final singleToneClass ourInstance = new singleToneClass();
-    public static singleToneClass getInstance() {
-        return ourInstance;
-    }
-    private singleToneClass() {
-    }
-    public void setData(String s) {
-        this.s = s;
-    }
-    public String getData() {
-        return s;
-    }
 }
